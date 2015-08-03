2c08adbf66fbee1eb263c8b48538bb235b3a7feb
Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2015-08-03 15:16:27 +0600

    Version 1

---
+++ src/com/example/appstest1/LastActivity.java
@@ -0,0 +1,78 @@
+package com.example.appstest1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+
+public class LastActivity extends ActionBarActivity {
+
+	TextView finalTextView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		try {
+			
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_last);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		finalTextView=(TextView) findViewById(R.id.textshowfinal);
+		
+		
+		finalTextView.setText("Welcome !! \n"+getIntent().getStringExtra("USER_NAME"));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.last, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_last, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
